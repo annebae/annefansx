9a85e6c84cf8ea1ee2ae13828298b2b7415df135
annebae <annebae69@outlook.com>
2024-12-31 07:35:04 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+function subscribe(profileName) {
+    const price = profileName === 'AnneBae' ? '20B$' : '20B$';
+    alert(`Thank you for subscribing to ${profileName}'s content! This will cost ${price} per month.`);
+}
